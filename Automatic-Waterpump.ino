d1080770b5eee6f96734c4b978b2fba415365cdc
aminnaz93 <Aminnazari040@hotmail.com>
2025-01-20 21:46:55 +0100

    Displaybranch

--- Automatic-Waterpump.ino
+++ Automatic-Waterpump.ino
@@ -1,12 +1,17 @@
+#include <Wire.h>             // För I2C-kommunikation
+#include <LiquidCrystal_I2C.h> // För LCD med I2C
 
-
+// Skapa ett LCD-objekt med adress 0x27 och en 16x2 display
+LiquidCrystal_I2C lcd(0x27, 16, 2);  
 
 void setup() {
-  // put your setup code here, to run once:
-
+  lcd.begin(16, 2);   // Initiera LCD med 16 kolumner och 2 rader
+  lcd.backlight();    // Tänd bakgrundsbelysning på displayen
+  
+  lcd.setCursor(0, 0); // Placera markören på rad 0, kolumn 0
+  lcd.print("carvajal"); // Skriv ut "Hello, World!" på LCD:n
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Inget mer behövs här för detta exempel
 }
